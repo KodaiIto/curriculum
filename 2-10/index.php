fc28288714e9a640516aab061d2bcf904f9f2624
KodaiIto <kidart0711@gmail.com>
2021-01-03 16:53:38 +0900

    更新

--- 2-10/index.php
+++ 2-10/index.php
@@ -2,7 +2,11 @@
 echo "＜課題＞" . '<br>';
 function Rectangular($length, $width, $height) {
     $volume  = $length * $width * $height;
+<<<<<<< Updated upstream
     print "直方体の体積は" . $volume . "です！";
+=======
+    print "直方体の体積は${volume}です！";
+>>>>>>> Stashed changes
 }
 Rectangular(5 , 10 , 8);
 echo '<br>';
