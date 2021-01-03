c067908c93f6d9af71f31958fded965938d2336f
KodaiIto <kidart0711@gmail.com>
2021-01-03 17:03:30 +0900

    フォーム内容取得の課題

--- 2-11/index.php
+++ 2-11/index.php
@@ -1,12 +1,16 @@
 <form action="result.php" method="post">
   名前：<input type="text" name="my_name" />
   <br>
-  メールアドレス：<input type="text" name="email" />
+  メールアドレス：<input type="email" name="email" />
   <br>
   パスワード：<input type="password" name="password" />
   <br>
   <input type="submit" value="送信" />
 </form>
+<!--今回は問題文に「inputタグのtype属性はtext」と指定があったので
+大正解ですが、 「input type="email"」と書くことができます。 
+こう書くと、メールアドレスの形式でない場合、
+自動でエラーを出してくれたりします。-->
 
 
 
--- 2-11/result.php
+++ 2-11/result.php
@@ -6,5 +6,9 @@ $password = $_POST['password'];
 <p>私の名前は、<?php echo $my_name; ?></p>
 <p>メールアドレスは、<?php echo $email; ?></p>
 <p>私のパスワードは、<?php echo $password; ?></p>
+<?php
+//var_dump($_POST);
+?>
+
 
 
