a754a4ccb567b92eb9af48c25fd5feb78d2f10a8
KodaiIto <kidart0711@gmail.com>
2021-01-03 16:51:11 +0900

    ブログを作ってみようの課題

--- 4-1/signUp.php
+++ 4-1/signUp.php
@@ -2,12 +2,20 @@
 require_once('db_connect.php');
 
 if(isset($_POST["signUp"])){
+<<<<<<< Updated upstream
     if( !empty($_POST["name"]) && !empty($_POST["password"])){
+=======
+    if(!empty($_POST["name"]) && !empty($_POST["password"])){
+>>>>>>> Stashed changes
         $name = $_POST["name"];
         $pass = $_POST["password"];
         $pdo = connect();
         try {
+<<<<<<< Updated upstream
             $sql = "INSERT INTO USERS(name,password) VALUE(:name,:password)";
+=======
+            $sql = "INSERT INTO USERS(name,password) VALUES(:name,:password)";
+>>>>>>> Stashed changes
             $password_hash = password_hash($pass, PASSWORD_DEFAULT);
             $stmt = $pdo->prepare($sql);
             $stmt->bindParam(':name', $name);
@@ -15,7 +23,11 @@ if(isset($_POST["signUp"])){
             $stmt->execute();
             echo  "登録完了！";
         } catch (PDOException $e) {
+<<<<<<< Updated upstream
             echo "Error:". $e->getMessage;
+=======
+            echo "Error:". $e->getMessage();
+>>>>>>> Stashed changes
             die();
         }
     } else {
@@ -34,11 +46,17 @@ if(isset($_POST["signUp"])){
     <h1>新規登録</h1>
     <form method="POST" action="">
         user:<br>
+<<<<<<< Updated upstream
         <input type="text" name="name" id="name">
         <br>
         password:<br>
         <input type="password" name="password" id="password">
         <br>
+=======
+        <input type="text" name="name" id="name"><br>
+        password:<br>
+        <input type="password" name="password" id="password"><br>
+>>>>>>> Stashed changes
         <input type="submit" value="submit" name ="signUp" id="signUp">
     </form>
 </body>
